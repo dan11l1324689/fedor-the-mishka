e5a4aec6812d547da7428468b42851ef790384ae
dan11l1324689 <90840580+dan11l1324689@users.noreply.github.com>
2022-04-05 17:04:20 +0500

    Add files via upload

--- fedor-the-mishka-main/script/AiEntity.gd
+++ fedor-the-mishka-main/script/AiEntity.gd
@@ -5,6 +5,7 @@ var chase_target
 var chase_timeout_time
 var patrol
 var target=Vector2()
+var chase_timeout_timer
 func update_target():
 	pass
 func update_final(_final):
@@ -48,15 +49,16 @@ func process_patrol():
 #нада заполниять  process_chase()
 func target_entity_position(entity : LivingEntity)-> Vector2:
 	return entity.position 
-
-func process_shase():
+func update_patrol_target():
+	var pre_target := partol_position + Vector2(rand_range(0, patrol_max_dis)
+func process_chase():
 	update_final (target_entity_position(chase_target))
 	update_target()
 	var new_chase_target_visibility : bool = check_visual_contact(chase_target)
 	if new_chase_target_visibility and not new_chase_target_visibility and chase_timeout_time != -1:
-		chase_timeout_timer.start(max(chase_timeout_time))
-	elif new_chase_chase_visibility:
+		chase_timeout_timer.start(max(chase_timeout_time, 0))
+	elif new_chase_target_visibility:
 		pass
 
 func chase_timeout():
-	chase_time_out
+		chase_timeout_timer.stop()
